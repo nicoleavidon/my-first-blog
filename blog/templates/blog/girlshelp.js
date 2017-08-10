5e33ad2127f0d3d0da782aacb85a36e4b99508f0
nicoleavidon <nicoleavidon@gmail.com>
2017-08-10 08:00:42 -0700

    Delete girlshelp.js

--- blog/templates/blog/girlshelp.js
+++
@@ -1,9 +0,0 @@
-function showProfileDropdown() {
-    document.getElementById("profileDropdown").classList.toggle("show");
-  }
-
-function showQuestion(){
-  var user_question = document.getElementById("question").value;
-  document.getElementById("printedquestion").innerHTML=user_question;
-
-}
